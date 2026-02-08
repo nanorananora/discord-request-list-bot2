3c1be4d039b03990736089d84659efc73765dd01
nanorananora <nanorananora8@gmail.com>
2026-02-08 20:58:31 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,18 @@
 import os
 import discord
 from discord.ext import commands
+import logging
+
+if os.getenv("ENABLE_UPDATE") != "true":
+    logging.info("更新は無効化されています")
+    return
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
+
+logging.info("Bot starting...")
 
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 if not TOKEN:
